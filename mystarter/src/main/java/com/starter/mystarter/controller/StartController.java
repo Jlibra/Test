0429236da02feff52b4eb09351ba02c4c182d369
jiangxf <jxinfeng@foxmail.com>
2020-03-26 17:47:25 +0800

    测试idea提交

--- mystarter/src/main/java/com/starter/mystarter/controller/StartController.java
+++ mystarter/src/main/java/com/starter/mystarter/controller/StartController.java
@@ -1,36 +1,36 @@
-//package com.starter.mystarter.controller;
-//
-//import com.starter.mystarter.service.MD5Service;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import javax.annotation.Resource;
-//
-///**
-// * <p>
-// * StartController:
-// * </p>
-// *
-// * @author jiangxf
-// * @version 2.8.0
-// * @since 2019-10-28
-// */
-//@Slf4j
-//@RequestMapping("ss")
-//public class StartController {
-//
-//    @Resource
-//    private MD5Service md5Service;
-//
-//    @RequestMapping("toPage/{md5}")
-//    public String toPage(@PathVariable("md5") String md5){
-//        log.info(md5Service.getMD5(md5));
-//        return "test1";
-//    }
-//
-//}
-//
-//
-//
-//
+package com.starter.mystarter.controller;
+
+import com.starter.mystarter.service.MD5Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>
+ * StartController:
+ * </p>
+ *
+ * @author jiangxf
+ * @version 2.8.0
+ * @since 2019-10-28
+ */
+@Slf4j
+@RequestMapping("ss")
+public class StartController {
+
+    @Resource
+    private MD5Service md5Service;
+
+    @RequestMapping("toPage/{md5}")
+    public String toPage(@PathVariable("md5") String md5){
+        log.info(md5Service.getMD5(md5));
+        return "test1";
+    }
+
+}
+
+
+
+
